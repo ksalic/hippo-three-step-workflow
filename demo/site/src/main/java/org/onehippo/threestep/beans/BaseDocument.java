3a1abaa550dda733c4373ddc8c569f69367c839a
Kenan Salic <k.salic@onehippo.com>
2016-04-11 13:48:44 +0200

    Initial commit

---
+++ demo/site/src/main/java/org/onehippo/threestep/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.threestep.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="threestepworkflowdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
